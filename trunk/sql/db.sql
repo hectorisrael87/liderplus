5143a95269b1226656ed17e8b5e9e08fa9ebd39f
ynfantes@gmail.com <ynfantes@gmail.com@063c7b17-da73-9b63-e1b0-2266273cdaa8>
2012-03-31 23:07:21 +0000

    Creacion la base de datos

--- trunk/sql/db.sql
+++ trunk/sql/db.sql
@@ -1,1 +1,381 @@
-/* Esquema de la base de datos */
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 01-04-2012 a las 00:57:49
+-- Versión del servidor: 5.1.58
+-- Versión de PHP: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT=0;
+START TRANSACTION;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `liderplus`
+--
+CREATE DATABASE `liderplus` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `liderplus`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(100) NOT NULL,
+  `apelidos` varchar(100) NOT NULL,
+  `tipo_documento_id` int(11) NOT NULL,
+  `numero_documento` varchar(24) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `numero_local` varchar(11) NOT NULL,
+  `numero_celular` varchar(11) NOT NULL,
+  `direccion` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tipo_documento_id` (`tipo_documento_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcar la base de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nombres`, `apelidos`, `tipo_documento_id`, `numero_documento`, `email`, `numero_local`, `numero_celular`, `direccion`) VALUES
+(1, 'Jose', 'Perez', 1, '4567890', 'ynfantes@gmail.com', '02129529955', '04129876534', 'Altos Mirandinos. Carrizal. Estado Mirando');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estatus_factura`
+--
+
+CREATE TABLE IF NOT EXISTS `estatus_factura` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcar la base de datos para la tabla `estatus_factura`
+--
+
+INSERT INTO `estatus_factura` (`id`, `descripcion`) VALUES
+(1, 'PENDIENTE'),
+(2, 'PAGADA');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estatus_pedido`
+--
+
+CREATE TABLE IF NOT EXISTS `estatus_pedido` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcar la base de datos para la tabla `estatus_pedido`
+--
+
+INSERT INTO `estatus_pedido` (`id`, `descripcion`) VALUES
+(1, 'PENDIENTE'),
+(2, 'EN PROCESO'),
+(3, 'PROCESADO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estatus_pedido_detalle`
+--
+
+CREATE TABLE IF NOT EXISTS `estatus_pedido_detalle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcar la base de datos para la tabla `estatus_pedido_detalle`
+--
+
+INSERT INTO `estatus_pedido_detalle` (`id`, `descripcion`) VALUES
+(1, 'PENDIENTE'),
+(2, 'PROCESADO'),
+(3, 'RECHAZADO');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE IF NOT EXISTS `factura` (
+  `id` int(11) NOT NULL,
+  `pedido_id` int(11) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `estatus_factura_id` int(11) NOT NULL,
+  `porcentaje_iva` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pedido_id` (`pedido_id`,`estatus_factura_id`),
+  KEY `estatus_factura_id` (`estatus_factura_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `factura`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura_detalle`
+--
+
+CREATE TABLE IF NOT EXISTS `factura_detalle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `factura_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` float NOT NULL,
+  `monto_iva` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `producto_id` (`producto_id`),
+  KEY `factura_id` (`factura_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `factura_detalle`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fase`
+--
+
+CREATE TABLE IF NOT EXISTS `fase` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `duracion` int(11) NOT NULL COMMENT 'especificada en horas',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Volcar la base de datos para la tabla `fase`
+--
+
+INSERT INTO `fase` (`id`, `nombre`, `duracion`) VALUES
+(1, 'Pedido', 10),
+(2, 'Almacén', 10),
+(3, 'Facturación', 10),
+(4, 'Chequeo', 10),
+(5, 'Transporte', 10),
+(6, 'Confirmación', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fase_grupo`
+--
+
+CREATE TABLE IF NOT EXISTS `fase_grupo` (
+  `fase_id` int(11) DEFAULT NULL,
+  `grupo_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `fase_grupo`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo`
+--
+
+CREATE TABLE IF NOT EXISTS `grupo` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Volcar la base de datos para la tabla `grupo`
+--
+
+INSERT INTO `grupo` (`id`, `descripcion`) VALUES
+(1, 'Super Usuario'),
+(2, 'Administrador'),
+(3, 'Vendedor'),
+(4, 'Almacenista'),
+(5, 'Cuentas por Cobrar'),
+(6, 'Despachador'),
+(7, 'Transportista'),
+(8, 'Conformador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE IF NOT EXISTS `pedido` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` varchar(10) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cliente_id` int(11) NOT NULL,
+  `estatus_pedido_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cliente_id` (`cliente_id`),
+  KEY `estatus_pedido_id` (`estatus_pedido_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `pedido`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido_detalle`
+--
+
+CREATE TABLE IF NOT EXISTS `pedido_detalle` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pedido_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `cantidad_pedido` int(11) NOT NULL DEFAULT '0',
+  `cantidad_despacho` int(11) NOT NULL DEFAULT '0',
+  `precio` float NOT NULL,
+  `estatus_pedido_detalle_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `producto_estatus_id` (`estatus_pedido_detalle_id`),
+  KEY `pedido_id` (`pedido_id`,`producto_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `pedido_detalle`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE IF NOT EXISTS `producto` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `codigo` varchar(12) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `precio` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `producto`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_documento`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_documento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(1) NOT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcar la base de datos para la tabla `tipo_documento`
+--
+
+INSERT INTO `tipo_documento` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'V', 'Venezolano'),
+(2, 'E', 'Extranjero'),
+(3, 'P', 'Pasaporte'),
+(4, 'J', 'Jurídico'),
+(5, 'G', 'Gobierno');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `grupo_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `grupo_id` (`grupo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `usuarios`
+--
+
+
+--
+-- Filtros para las tablas descargadas (dump)
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`tipo_documento_id`) REFERENCES `tipo_documento` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `factura_ibfk_2` FOREIGN KEY (`estatus_factura_id`) REFERENCES `estatus_factura` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `factura_detalle`
+--
+ALTER TABLE `factura_detalle`
+  ADD CONSTRAINT `factura_detalle_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `factura_detalle_ibfk_1` FOREIGN KEY (`factura_id`) REFERENCES `factura` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`estatus_pedido_id`) REFERENCES `estatus_pedido` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pedido_detalle`
+--
+ALTER TABLE `pedido_detalle`
+  ADD CONSTRAINT `pedido_detalle_ibfk_2` FOREIGN KEY (`estatus_pedido_detalle_id`) REFERENCES `estatus_pedido_detalle` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `pedido_detalle_ibfk_1` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`grupo_id`) REFERENCES `grupo` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
